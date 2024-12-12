d0ab671dbef3d76f3cad162adc006d7a2ce257f8
Ali Mousavi <asam3000m@gmail.com>
2024-12-12 18:17:15 +0330

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./layouts/panel/layout/layout.component').then(
+        (c) => c.LayoutComponent
+      ),
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./pages/dashboard/dashboard.component').then(
+            (c) => c.DashboardComponent
+          ),
+      },
+    ],
+  },
+  {
+    path: 'auth',
+    loadComponent: () =>
+      import('./layouts/auth/layout/layout.component').then(
+        (c) => c.LayoutComponent
+      ),
+  },
+];
---
+++ src/app/layouts/panel/layout/layout.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-layout',
+  standalone: true,
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.css'],
+  imports: [HeaderComponent, FooterComponent, RouterOutlet],
+})
+export class LayoutComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
