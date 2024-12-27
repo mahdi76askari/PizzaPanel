b3ed2c39eb04d2d921be8e08900098afae0ef1f2
Ali Mousavi <asam3000m@gmail.com>
2024-12-27 21:20:14 +0330

    add branch

---
+++ src/app/pages/branch-panel/my-history/my-history.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-history',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-history.component.html',
+  styleUrl: './my-history.component.scss'
+})
+export class MyHistoryComponent {
+
+}
