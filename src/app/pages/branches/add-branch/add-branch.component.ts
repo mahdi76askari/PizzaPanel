1c8044fd9eafb7d64d0020732bee20b87ecf3132
Ali Mousavi <asam3000m@gmail.com>
2025-02-01 10:57:31 +0330

    add branch update

--- src/app/pages/branches/add-branch/add-branch.component.ts
+++ src/app/pages/branches/add-branch/add-branch.component.ts
@@ -1,12 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { InputTextComponent } from '../../../components/elements/forms/input-text/input-text.component';
+import { SelectComponent } from '../../../components/elements/forms/select/select.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgxMaskDirective } from 'ngx-mask';
 
 @Component({
   selector: 'app-add-branch',
   standalone: true,
   templateUrl: './add-branch.component.html',
   styleUrls: ['./add-branch.component.css'],
+  imports: [
+    InputTextComponent,
+    SelectComponent,
+    NgxMaskDirective,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
 })
 export class AddBranchComponent implements OnInit {
+  saturdayMorningFrom = '09:00';
+  saturdayMorningTo = '12:00';
   constructor() {}
 
   ngOnInit() {}
